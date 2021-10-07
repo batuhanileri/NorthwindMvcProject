9772b4ed7d0eb6c3401c38987fee94bf0a17da72
batuhanileri <bileriofficial@gmail.com>
2021-10-07 17:39:31 +0300

    Wcf Added,Modified IISHost <- WcfLibrary Reference

--- NorthwindInterfaces/IProductService.cs
+++ NorthwindInterfaces/IProductService.cs
@@ -2,17 +2,27 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NorthwindInterfaces
 {
+    [ServiceContract]
     public interface IProductService
     {
+        [OperationContract]
         List<Product> GetAll();
+
+        [OperationContract]
         Product GetById(int productId);
+
+        [OperationContract]
         void Add(Product product);
+        [OperationContract]
         void Delete(int productId);
+
+        [OperationContract]
         void Update(Product product);
 
     }
---
+++ WcfLibrary/Concrete/ProductService.cs
@@ -0,0 +1,42 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete.EntityFramework;
+using EntitiesLayer;
+using NorthwindInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfLibrary.Concrete
+{
+    public class ProductService : IProductService
+    {
+        private ProductManager _productManager = new ProductManager(new EfProductDal());
+        public void Add(Product product)
+        {
+            _productManager.Add(product);
+        }
+
+        public void Delete(int productId)
+        {
+            _productManager.Delete(productId);
+
+        }
+        public List<Product> GetAll()
+        {
+            return _productManager.GetAll();
+        }
+
+        public void Update(Product product)
+        {
+            _productManager.Update(product);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productManager.GetById(productId);
+
+        }
+    }
+}
